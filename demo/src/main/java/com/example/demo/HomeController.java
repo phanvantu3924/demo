232111c12c7cde34de6a90f2b916c995660f768d
phanvantu3924 <phantu3924@gmail.com>
2025-08-22 21:21:43 +0700

    Cập nhật code mới và chỉnh sửa chi tiết

--- demo/src/main/java/com/example/demo/HomeController.java
+++ demo/src/main/java/com/example/demo/HomeController.java
@@ -1,4 +1,4 @@
-package main.java.com.example.demo;
+package com.example.demo;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
--- demo/src/main/java/com/example/demo/SecurityConfig.java
+++ demo/src/main/java/com/example/demo/SecurityConfig.java
@@ -1,29 +1,14 @@
-package main.java.com.example.demo;
+package com.example.demo;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-
-    // Khai báo user test
-    @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails user = User.withDefaultPasswordEncoder()
-            .username("admin")
-            .password("123456")
-            .roles("USER")
-            .build();
-
-        return new InMemoryUserDetailsManager(user);
-    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -37,8 +22,7 @@ public class SecurityConfig {
                 .permitAll()
             )
             .logout(logout -> logout.permitAll());
-
+        // CSRF bật mặc định
         return http.build();
     }
 }
-
